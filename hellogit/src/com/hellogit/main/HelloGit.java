4ddf7bb1cb6713739416a6033c8ede02df71a6b8
ramesh <ramesh@HASINI>
2021-03-31 19:58:23 +0530

    changed code

--- hellogit/src/com/hellogit/main/HelloGit.java
+++ hellogit/src/com/hellogit/main/HelloGit.java
@@ -3,7 +3,7 @@ package com.hellogit.main;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git. Nice to know you!!");
+		System.out.println("Hello Git. Nice to know you!!. I am excited to work with you");
 	}
 
 }
